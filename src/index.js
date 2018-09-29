54e2dda3e8ee4be80ce203ba040f1ccbced5655b
sergei yezhgunovich <f.glomgold@yandex.ru>
2018-09-29 18:07:25 +0300

    countZer0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let factor=primeSearch(base);
+  let maxFactor=searchMaxFactor(factor);
+  let countZero=0;
+  const MAXFACTOR=maxFactor[0];
+  while(maxFactor[0]<number){
+      countZero+=Math.floor(number/maxFactor[0]);
+      maxFactor[0]*=MAXFACTOR;
+  }
+  return Math.floor(countZero/maxFactor[1]);
+
+}   
+
+function searchMaxFactor(factor){
+  let maxFactor=0;
+  for(key in factor) 
+      if(+key>+maxFactor)
+          maxFactor=key; 
+  return  [[maxFactor],factor[maxFactor]];
 }
+
+function primeSearch(base){
+  let primeBase=base;
+  let factor={};
+  let i;
+  while(primeBase!=1){
+      for(i=2;primeBase%i!=0;i++);
+      if(factor[i]==undefined) factor[i]=1;
+      else factor[i]++;     
+      primeBase/=i;
+  }
+  return factor;
+}
+
+
